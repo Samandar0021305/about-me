ca1641ca4ccee7663411a5e518aabc33e353508a
Samandar0021305 <samandar021325@gmail.com>
2022-08-03 14:25:09 +0500

    init

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var navbar = document.getElementById("navbar");
+navbar.style.background = "#111";
+navbar.style.width = "100%";
+navbar.style.height = "70px";
+
+var navbarText = document.getElementById("navbarText")
+navbarText.style.color = "white";
+navbarText.textContent = "Samandar";
