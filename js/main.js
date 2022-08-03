70a74376dd43e4226316c4a9dfea3dcabfb624e4
Samandar0021305 <samandar021325@gmail.com>
2022-08-03 14:30:32 +0500

    init

--- js/main.js
+++ js/main.js
@@ -6,3 +6,18 @@ navbar.style.height = "70px";
 var navbarText = document.getElementById("navbarText")
 navbarText.style.color = "white";
 navbarText.textContent = "Samandar";
+
+let navbarBox = document.querySelector(".navbar-box")
+let navbarUlElement = document.createElement("ul");
+let navbarListArry = ["Home","About","Contacts"];
+
+for(let item of navbarListArry){
+    let navbarLiElement = document.createElement("li")
+    let navbarLinkElement  = document.createElement("a")
+    navbarLinkElement.textContent = item;
+    navbarLiElement.appendChild(navbarLinkElement)
+    navbarUlElement.appendChild(navbarLiElement)
+}
+
+navbarBox.appendChild(navbarUlElement)
+
