fcee6e1c1480bb085d71f6f72ff6a83f22cb9ad6
Samandar0021305 <samandar021325@gmail.com>
2022-09-29 08:40:13 +0500

    init

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var navbar__toogle = document.querySelector(".navbar__toogle");
+var navbar__toogle__bg = document.querySelector(".navbar__toogle__bg");
+var navbar__close = document.querySelector(".navbar__close")
+var header = document.querySelector(".header")
+if(!navbar__toogle){
+    navbar__toogle__bg.classList.remove("navbar__open")
+}
+if(navbar__toogle){
+    navbar__toogle.addEventListener("click",()=>{
+        navbar__toogle__bg.classList.toggle("navbar__open")
+    })
+}
+navbar__close.addEventListener("click",()=>{
+    navbar__toogle__bg.classList.remove("navbar__open")
+})
+
+
+header.addEventListener("click",()=>{
+    navbar__toogle__bg.classList.remove("navbar__open")
+})
